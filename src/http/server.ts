25b3f5185f126654c87e3938019d6aed15dd9593
GiovanaNapoli <giovana.ap.napoli@outlook.com>
2025-04-29 20:05:44 -0300

    feat: add cors

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import env from "../env";
 import route from "./route";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 
 app.use(route);
+app.use(cors());
 
 app.listen(env.PORT, () => {
   console.log(`Server is running on port ${env.PORT}`);
