dc345d90649dbbfacd01e75a650a0b23cab1e41e
GiovanaNapoli <giovana.ap.napoli@outlook.com>
2025-04-28 21:10:51 -0300

    fix: fix port

--- src/http/server.ts
+++ src/http/server.ts
@@ -7,6 +7,6 @@ app.use(express.json());
 
 app.use(route);
 
-app.listen(env.LOCAL_PORT, () => {
-  console.log(`Server is running on port ${env.LOCAL_PORT}`);
+app.listen(3333, () => {
+  console.log(`Server is running on port ${3333}`);
 });
