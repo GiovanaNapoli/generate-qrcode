0da889c5c56e64ffa6ad4c2d77db34de275d5101
Giovana Aparecida Napoli da Silva <32879408+GiovanaNapoli@users.noreply.github.com>
2025-04-29 09:14:12 -0300

    fix: change port localhost

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,11 +2,13 @@ import express from "express";
 import env from "../env";
 import route from "./route";
 
+const port = env.PORT || 3333;
+
 const app = express();
 app.use(express.json());
 
 app.use(route);
 
-app.listen(env.PORT, () => {
-  console.log(`Server is running on port ${env.PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
