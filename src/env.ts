5d629c8357d8402539e99d44081ed55100150327
GiovanaNapoli <giovana.ap.napoli@outlook.com>
2025-04-28 21:40:13 -0300

    fix: localhost port

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,7 @@
 import z from "zod";
 
 const envSchema = z.object({
-  LOCAL_PORT: z
-    .preprocess((a) => parseInt(a as string), z.number())
-    .default(3333),
+  PORT: z.preprocess((a) => parseInt(a as string), z.number()).default(3333),
 });
 
 const env = envSchema.parse(process.env);
--- src/http/server.ts
+++ src/http/server.ts
@@ -7,6 +7,6 @@ app.use(express.json());
 
 app.use(route);
 
-app.listen(3333, () => {
-  console.log(`Server is running on port ${3333}`);
+app.listen(env.PORT, () => {
+  console.log(`Server is running on port ${env.PORT}`);
 });
