e6d0e7d74441fdb6685eecae7c2e04fc963625ef
GiovanaNapoli <giovana.ap.napoli@outlook.com>
2025-04-29 20:34:43 -0300

    fix: fixing cors error

--- src/http/server.ts
+++ src/http/server.ts
@@ -8,15 +8,16 @@ const port = env.PORT || 3333;
 const app = express();
 app.use(express.json());
 
-app.use(route);
-app.use(cors());
+const corsOptions = {
+  AccessControlAllowOrigin: "*",
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+app.use(cors(corsOptions));
+
+app.use(route);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
