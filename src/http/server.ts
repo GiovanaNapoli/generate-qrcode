5a792ec600b7c5712014585dd3d713c2a33156c2
GiovanaNapoli <giovana.ap.napoli@outlook.com>
2025-04-29 20:13:10 -0300

    feat: add middleware

--- src/http/server.ts
+++ src/http/server.ts
@@ -11,6 +11,13 @@ app.use(express.json());
 app.use(route);
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
